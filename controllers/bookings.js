2fed4343e4b8e0e2808f5f06a48bc8410fa6323e
Aquaturner <paritleo@gmail.com>
2024-03-02 23:24:12 +0700

    Update bookings.js

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -62,3 +62,111 @@ exports.getBooking = async(req,res,next)=>{
         return res.status(500).json({success:false, message : "Cannot find a booking"})
     }
 };
+
+//@desc     Add booking
+//@route    POST /api/v1/bookings/:carId/booking
+//@access   Private
+exports.addBooking = async (req, res, next) => {
+    try {
+        req.body.car = req.params.carId;
+
+        const car = await Car.findById(req.params.carId);
+
+        if (!car) {
+            return res.status(404).json({
+                success: false,
+                message: `No car with the id of ${req.params.carId}`
+            });
+        }
+
+        //add user Id to req.body
+        req.body.user = req.user.id;
+        
+        //Check for existed booking
+        const existedBookings = await Booking.find({user: req.user.id});
+        
+        //If the user is not an admin, they can only create 3 bookings
+        if (existedBookings.length >= 3 && req.user.role !== 'admin') {
+            return res.status(404).json({
+                success: false,
+                message: `The user with ID ${req.user.id} has already made 3 bookings`
+            });
+        }
+
+        const booking = await Booking.create(req.body);
+
+        res.status(200).json({
+            success: true,
+            data: booking
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot create Booking"
+        });
+    }
+}
+
+//@desc     Update booking
+//@route    PUT /api/v1/bookings/:id
+//@access   Private
+exports.updateBooking = async (req, res, next) => {
+    try {
+        let booking = await Booking.findById(req.params.id);
+
+        //Make sure user is the booking owner
+        if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success: false,
+                message: `User ${req.user.id} is not authorized to update this booking`
+            });
+        }
+
+        booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({
+            success: true,
+            data: booking
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot update Booking"
+        });
+    }
+};
+
+//@desc     Delete booking
+//@route    DELETE /api/v1/bookings/:id
+//@access   Private
+exports.deleteBooking = async (req, res, next) => {
+    try {
+        const booking = await Booking.findById(req.params.id);
+
+        //Make sure user is the booking owner
+        if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success: false,
+                message: `User ${req.user.id} is not authorized to delete this booking`
+            });
+        }
+
+        await booking.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot delete booking"
+        });
+    }
+}
